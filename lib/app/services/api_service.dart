8757764309d8d81140dde3a76364b7af33dbf7c7
Ayas Nasih <ayasnasih@gmail.com>
2020-10-15 01:08:41 +0500

    Retrieves access token or throws an error if it fails

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:corona_app/app/services/api.dart';
 
@@ -13,5 +15,15 @@ class APIService {
         'Authorization': 'Basic ${api.apiKey}',
       },
     );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+        'Request ${api.tokenUri()} failed \n Response: ${response.statusCode} ${response.reasonPhrase}');
+    throw response;
   }
 }
