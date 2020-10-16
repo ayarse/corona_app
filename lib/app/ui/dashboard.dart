0d9efb6318c90e33a69c719f6e8708182f03384a
Ayas Nasih <ayasnasih@gmail.com>
2020-10-17 03:19:24 +0500

    Begin new UI

---
+++ lib/app/ui/dashboard.dart
@@ -0,0 +1,46 @@
+import 'package:corona_app/app/repositories/data_repository.dart';
+import 'package:corona_app/app/services/api.dart';
+import 'package:corona_app/app/ui/endpoint_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  int _cases;
+
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    final cases = await dataRepository.getEndpointData(Endpoint.cases);
+    setState(() => _cases = cases);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _updateData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Coronavirus Tracker'),
+        ),
+        body: RefreshIndicator(
+          onRefresh: _updateData,
+          child: ListView(
+            children: <Widget>[
+              EndpointCard(
+                endpoint: Endpoint.cases,
+                value: _cases,
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/app/ui/endpoint_card.dart
@@ -0,0 +1,32 @@
+import 'package:corona_app/app/services/api.dart';
+import 'package:flutter/material.dart';
+
+class EndpointCard extends StatelessWidget {
+  final Endpoint endpoint;
+  final int value;
+
+  const EndpointCard({Key key, this.endpoint, this.value}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Cases',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Text(
+                value != null ? value.toString() : '',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
