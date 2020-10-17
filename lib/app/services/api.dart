e9e1503623b0dd527a17596ddbf4cbaa203a08d6
Ayas Nasih <ayasnasih@gmail.com>
2020-10-18 03:30:57 +0500

    Moves API keys to env file

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,5 +1,5 @@
-import 'package:corona_app/app/services/api_keys.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 enum Endpoint {
   cases,
@@ -13,7 +13,7 @@ class API {
   API({@required this.apiKey});
   final String apiKey;
 
-  factory API.sandbox() => API(apiKey: APIKeys.ncovSandboxKey);
+  factory API.sandbox() => API(apiKey: DotEnv().env['SANDBOX_KEY']);
 
   static final String host = 'ncov2019-admin.firebaseapp.com';
 
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:corona_app/app/services/api_service.dart';
 import 'package:corona_app/app/services/data_cache_service.dart';
 import 'package:corona_app/app/ui/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -14,6 +15,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Intl.defaultLocale = 'en_GB';
   await initializeDateFormatting();
+  await DotEnv().load('.env');
   final sharedPreferences = await SharedPreferences.getInstance();
   runApp(MyApp(sharedPreferences: sharedPreferences,));
 }
